db5e7590a2feac00cc8356d5061e3eed7705e7ce
Ellianel <michal93nieklan@gmail.com>
2022-11-29 13:50:28 +0100

    added  Home component and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, Link } from "react-router-dom";
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom'
+import Home from './components/Home/Home-component'
 
 function App() {
-  return (
-          <div>
-              Hello
-          </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />}></Route>
+			</Routes>
+			<h2>hello</h2>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
