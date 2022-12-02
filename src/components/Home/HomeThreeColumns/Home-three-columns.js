1c6736b8f2b3a8d44eb287978d16cd3b815bc09e
LuckiMoH <mat.maj1992@gmail.com>
2022-12-02 11:49:08 +0100

    Three columns component done

--- src/components/Home/HomeThreeColumns/Home-three-columns.js
+++ src/components/Home/HomeThreeColumns/Home-three-columns.js
@@ -4,19 +4,19 @@ const HomeThreeColumns = () => {
     return (
         <section className='three-columns-wrapper' id='three-columns'>
             <div className="column">
-                <h2>10</h2>
+                <h2 className='column-bags-amount'>10</h2>
                 <h3>Oddanych worków</h3>
-                <div className="column-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem dolores dolorum excepturi expedita maxime rerum sint totam. Deleniti, quis?</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem dolores dolorum excepturi expedita maxime rerum sint totam. Deleniti, quis?</p>
             </div>
             <div className="column">
-                <h2>5</h2>
+                <h2 className='column-organization-amount'>5</h2>
                 <h3>Wspartych organizacji</h3>
-                <div className="column-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolor doloremque dolores dolorum eligendi harum ipsum laudantium sed, veniam veritatis!</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolor doloremque dolores dolorum eligendi harum ipsum laudantium sed, veniam veritatis!</p>
             </div>
             <div className="column">
-                <h2>7</h2>
+                <h2 className='column-giveaways-amount'>7</h2>
                 <h3>Zorganizowanych zbiórek</h3>
-                <div className="column-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur culpa hic laborum magnam minima quaerat qui repellat saepe sint velit.</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur culpa hic laborum magnam minima quaerat qui repellat saepe sint velit.</p>
             </div>
         </section>
     );
