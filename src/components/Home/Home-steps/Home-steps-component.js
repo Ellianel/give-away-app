0ede90612d879913d8906d918f5757f0409af77f
PiotrStch <105080464+PiotrStch@users.noreply.github.com>
2022-12-03 11:13:06 +0100

    icon_work

--- src/components/Home/Home-steps/Home-steps-component.js
+++ src/components/Home/Home-steps/Home-steps-component.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import decoration from "../../../assets/Decoration.svg";
+import icon1 from "../../../assets/Icon1.svg";
 
 const HomeStepsComponent = () => {
-    return (
-        <h2 id='steps'>
-           STEPS
-        </h2>
-    );
+	return (
+		<section>
+			<h2 id='steps'>Wystarczą 4 proste kroki</h2>
+			<img src={decoration} alt='decoration-line' />
+			<div>
+				<div> 
+					<img src={icon1} alt='icon-wybierz_rzeczy' /> 
+				</div>
+
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</section>
+	);
 };
 
 export default HomeStepsComponent;
