1b147e50ca1dd7e7875050ee71fa836f76d09533
LuckiMoH <mat.maj1992@gmail.com>
2022-12-08 19:43:45 +0100

    Form section done without logic to send data to API

--- src/Register-LogIn-Page/FormRow.js
+++ src/Register-LogIn-Page/FormRow.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const FormRow = ({type, name, value, handleChange, labelText}) => {
+const FormRow = ({type, name, value, handleChange, placeholder, labelText}) => {
     return (
         <div className="form-row">
             <label htmlFor={name} className='form-label'>{labelText || name}</label>
-            <input id={name} type={type} name={name} value={value} onChange={handleChange} className='form-input'/>
+            <input id={name} type={type} name={name} value={value} placeholder={placeholder} onChange={handleChange} className='form-input'/>
         </div>
     );
 };
--- src/components/Home/Home-contact/Home-contact-component.jsx
+++ src/components/Home/Home-contact/Home-contact-component.jsx
@@ -1,16 +1,64 @@
 import React from "react";
+import { useState } from "react";
 import decoration from "../../../assets/Decoration.svg";
+import FormRow from "../../../Register-LogIn-Page/FormRow";
+
+const initialState = {
+  name: "",
+  email: "",
+  textValue:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+};
 
 export const HomeContactComponent = () => {
-	return (
-		<section className='home-contact'>
-			<div className='overlay'>
-				<div>Skontaktuj się z nami</div>
-				<img className='deco' src={decoration} alt='decoration-line' />
-				<form>
-					<button></button>
-				</form>
-			</div>
-		</section>
-	);
+  const [values, setValues] = useState(initialState);
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+  };
+  return (
+    <section className="home-contact">
+      <div className="overlay">
+        <div className="contact-wrapper">
+          <h3>Skontaktuj się z nami</h3>
+          <img className="deco" src={decoration} alt="decoration-line" />
+          <form className="contact-form">
+            <div className="contact-form-row">
+              <FormRow
+                type="text"
+                name="name"
+                labelText="Wpisz swoje imię"
+                placeholder='Krzysztof'
+                value={values.name}
+                handleChange={handleChange}
+              />
+              <FormRow
+                type="email"
+                name="email"
+                labelText="Wpisz swój email"
+                placeholder='abc@xyz.pl'
+                value={values.email}
+                handleChange={handleChange}
+              />
+            </div>
+            <div className="contact-form-textarea">
+            {/* <FormRow
+                type="textarea"
+                name="textValue"
+                labelText="Wpisz swoją wiadomość"
+                value={values.textValue}
+                handleChange={handleChange}
+              /> */}
+              <label for="textValue" class="form-label">
+                Wpisz swoją wiadomość
+              </label>
+              <textarea id="textValue" placeholder={values.textValue}></textarea>
+            </div>
+            <button className="contact-btn">Wyślij</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
